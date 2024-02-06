039674bfe426d6d998911561c947e04e4fcbd136
EBallico <eduardoballico.dev@gmail.com>
2024-02-05 23:22:34 -0300

    Feature: Creating banner component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,17 @@
 <template>
-  <h1>Meu primeiro projeto Vue!</h1>
+  <Banner />
 </template>
+
+<script lang="ts">
+import Banner from './components/Banner.vue';
+
+export default {
+  components: {
+    Banner,
+  },
+};
+
+</script>
+<style scoped>
+
+</style>
